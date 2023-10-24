07d5257ec0495d38ae1afe097471f9e0d184fcd7
Rafaela Wolf <rafaelawolf52@gmail.com>
2023-10-24 20:31:53 -0300

    trabalhando no js

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,23 @@
-const btnAdviceGenerator = document.getElementById("btn-advice");
+// 1. Pegar o elemento de  **botão** para ser adicionado o **evento de clique** nele.
+const btnAdviceGenerator = document.querySelector("btn-advice");
 
-document.getElementsById("advice-number").innerHTML = adviceNumber;
+// 2. Pegar o elemento de **id do conselho**
+const adviceNumber = document.getElementById("advice-number");
 
-const adviceDescription = document.getElementsByClassName('.advice');
+// 3. Pegar o elemento de **descrição do conselho**
+const adviceDescription = document.getElementById("advice");
 
-async function randomAdvice() {
+// 4. Após conseguirmos os elementos do html, é necessário a criação da **função responsável por pegar os dados da API**. A melhor forma de se consumir a api é usando **async await**. Caso não tenha visto a aula, sugiro que assista novamente. Nas referências ficará um link com exemplos.
+async function gerarConselhos() {
     const url =
       "https://api.adviceslip.com/advice";
-    const resposta = await fetch(url);
-    return await resposta.json();
+      const resposta = await fetch(url);
+      return await resposta.json();
+    // 5. Dentro da função, após pegarmos os dados da API, é necessário atualizar o conteúdo do id e descrição do conselho que colocamos em uma variável usando o **innerHTML**.
 }
 
-async function getRandomAdvice(slip_id) {
-    const url = `https://api.adviceslip.com/advice/${slip_id}`;
-    const resposta = await fetch(url);
-    return await resposta.json();
-}
-  
-async function printRandomAdvice() {
-    const advice = await randomAdvice();
-    const giveRandomAdvice = await getRandomAdvice(advice.slip_id);
-    console.log(giveRandomAdvice);
-}
+// 6. Por fim, basta chamar a função criada.
+gerarConselhos()
+
+// 7. Atribuir a função que criamos para ser executada no evento do clique do botão ou se preferir, criar o escopo da função dentro do listener.
+btnAdviceGenerator.addEventListener('click', gerarConselhos());
